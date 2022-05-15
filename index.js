9e67d6ea0e5ce66c37d9551081e8c17ddff9e677
Rutvik Mendpara <86140002+RutvikMendpara@users.noreply.github.com>
2022-05-15 11:01:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const randomNumber1 = Math.floor(Math.random() * 6 ) + 1;
 const randomNumber2 = Math.floor(Math.random() * 6 ) + 1;
 
-document.querySelector(".img1").setAttribute("src","/images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","/images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
 
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent = "👦 won the game";
